1176e4f4c0aea7122c0666220edfd76b62b320d8
Lorenzo Lamberti <lorenzo.lamberti@unibo.it>
2022-08-03 06:12:15 -0700

    WORKING: Lorenzo s collision avoidance. initial delay_till_no_collisions_occur

--- mrim_task/mrim_planner/scripts/trajectory.py
+++ mrim_task/mrim_planner/scripts/trajectory.py
@@ -607,7 +607,7 @@ class TrajectoryUtils():
         ## |  [COLLISION AVOIDANCE METHOD #2]: Delay UAV with shorter trajectory at start until there is no collision occurring  |
         elif method == 'delay_till_no_collisions_occur':
 
-            raise NotImplementedError('[STUDENTS TODO] Collision prevention method \'delay_till_no_collisions_occur\' not finished. You have to finish it on your own.')
+            # raise NotImplementedError('[STUDENTS TODO] Collision prevention method \'delay_till_no_collisions_occur\' not finished. You have to finish it on your own.')
             # Tips:
             #  - you might select which trajectory it is better to delay
             #  - the smallest delay step is the sampling step stored in variable 'self.dT'
@@ -615,26 +615,46 @@ class TrajectoryUtils():
             delay_step = self.dT
             traj_times = [t.getTime() for t in trajectories]
             traj_lens  = [t.getLength() for t in trajectories]
-
+            # print('DEBUG:',traj_times, '-------------------------------')
             # Decide which UAV should be delayed
             # [STUDENTS TODO] CHANGE BELOW
             delay_robot_idx, nondelay_robot_idx = 0, 1
+
+            #### Lorenzo's implementation ####
+            traj_time_min_index = np.argmin(traj_times)
+            traj_time_max_index = np.argmax(traj_times)
+            delay_robot_idx, nondelay_robot_idx = traj_time_min_index, traj_time_max_index
+            ##################################
+
 
             # TIP: use function `self.trajectoriesCollide()` to check if two trajectories are in collision
             collision_flag, collision_idx = \
                 self.trajectoriesCollide(trajectories[delay_robot_idx], trajectories[nondelay_robot_idx], safety_distance)
 
-            i = 0
+            i=0
             while collision_flag:
 
-                # delay the shorter-trajectory UAV at the start point by sampling period
-
-                if i < 10:
+                if collision_flag:
                     delay_t += delay_step
-                    # TIP: use function `trajectory.delayStart(X)` to delay a UAV at the start location by X seconds
-                    trajectories[delay_robot_idx].delayStart(delay_step)
-                else:
-                    collision_flag = False
+                else: break
+
+                trajectories[delay_robot_idx].delayStart(delay_t)
+
+                collision_flag, collision_idx = \
+                    self.trajectoriesCollide(trajectories[delay_robot_idx], trajectories[nondelay_robot_idx], safety_distance)
+                i+=1
+
+            # i = 0
+            # while collision_flag:
+
+            #     # delay the shorter-trajectory UAV at the start point by sampling period
+
+            #     if i < 10:
+            #         delay_t += delay_step
+            #         # TIP: use function `trajectory.delayStart(X)` to delay a UAV at the start location by X seconds
+            #         trajectories[delay_robot_idx].delayStart(delay_step)
+            #     else:
+            #         collision_flag = False
 
         # # #}
 
